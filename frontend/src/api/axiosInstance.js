2f78493f44b8a9d3f01664797912c15c272dfca5
Awel-Abduljelil <awela1499@gmail.com>
2026-08-29 11:21:22 +0300

    servers setup for render

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://astumsj-bootcamp-management-system.vercel.app/",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
 });
 axiosInstance.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
