b1d8cf27536eca89244e1cb81ec60d54ff62bf5a
Seid Star <seid@gmail.com>
2026-08-29 20:14:54 +0300

    add dashboard API integration

--- frontend/src/api/dashboard.api.js
+++ frontend/src/api/dashboard.api.js
@@ -1,5 +1,11 @@
 import axiosInstance from "./axiosInstance";
+
+export const getPublicDashboard = () => axiosInstance.get("/dashboard/public");
+export const getPublicMentors = () =>
+  axiosInstance.get("/dashboard/public/mentors");
 export const getAdminDashboard = () => axiosInstance.get("/dashboard/admin");
+
 export const getMentorDashboard = () => axiosInstance.get("/dashboard/mentor");
+
 export const getStudentDashboard = () =>
   axiosInstance.get("/dashboard/student");
