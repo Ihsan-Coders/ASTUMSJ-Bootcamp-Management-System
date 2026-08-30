52a2951d74ee6846bedb88e6585aee572fc5861a
Awel-Abduljelil <awela1499@gmail.com>
2026-08-30 07:21:30 +0300

    file upload

--- backend/src/routes/batch.routes.js
+++ backend/src/routes/batch.routes.js
@@ -4,7 +4,7 @@ const protect = require("../middleware/auth.middleware");
 const authorize = require("../middleware/role.middleware");
 const {
   createBatch, getBatches, getOpenBatches, updateBatch, deleteBatch,
-  assignMentorToBatch, enrollStudentInBatch, setAcceptingBatch,
+  assignMentorToBatch, enrollStudentInBatch, assignMentorToStudent, setAcceptingBatch,
 } = require('../controllers/batch.controller');
 const validate = require('../middleware/validate.middleware');
 const { createBatchSchema, updateBatchSchema } = require('../validators/batch.validator');
@@ -17,6 +17,7 @@ router.put('/:id', authorize('admin'),validate(updateBatchSchema), updateBatch);
 router.delete('/:id', authorize('admin'), deleteBatch);
 router.post('/assign-mentor', authorize('admin'), assignMentorToBatch);
 router.post('/enroll-student', authorize('admin'), enrollStudentInBatch);
+router.post('/assign-mentor-to-student', authorize('admin'), assignMentorToStudent);
 router.put('/:id/set-accepting', authorize('admin'), setAcceptingBatch);
 
 module.exports = router;
